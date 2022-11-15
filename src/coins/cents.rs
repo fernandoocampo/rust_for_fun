5b19708a09b09da5fdda30b94fd1a8d7568e2e21
fernando ocampo <fdocampo@gmail.com>
2022-11-15 22:24:46 +0100

    add example with match and option

--- src/coins/cents.rs
+++ src/coins/cents.rs
@@ -1,8 +1,16 @@
+#[derive(Debug)]
+pub enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Kentucky,
+}
+
 pub enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 pub fn value_in_cents(coin: Coin) -> u8 {
@@ -10,6 +18,16 @@ pub fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+pub fn plus_one(value: Option<i32>) -> Option<i32> {
+    match value {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
--- tests/coins_test.rs
+++ tests/coins_test.rs
@@ -10,3 +10,19 @@ fn test_value_in_cents() {
     // THEN
     assert_eq!(got, expected_result);
 }
+
+#[test]
+fn test_plus_one() {
+    // GIVEN
+    let given_value_one = Some(5);
+    let given_value_two = None;
+
+    let expected_result_one = Some(6);
+    let expected_result_two = None;
+    // WHEN
+    let got_one = cents::plus_one(given_value_one);
+    let got_two = cents::plus_one(given_value_two);
+    // THEN
+    assert_eq!(got_one, expected_result_one);
+    assert_eq!(got_two, expected_result_two);
+}
