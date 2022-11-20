3a22938e9478db9e2cbbe6dc0bcec1058cd7cb7a
fernando ocampo <fdocampo@gmail.com>
2022-11-20 19:33:04 +0100

    add welcome example

--- src/greetings/hi.rs
+++ src/greetings/hi.rs
@@ -1,3 +1,10 @@
 pub fn hello() -> String {
     String::from("Hello")
 }
+
+pub fn welcome(name: &str) -> String {
+    let mut welcome = String::from("Welcome ");
+    welcome.push_str(name);
+
+    welcome
+}
--- tests/greetings_test.rs
+++ tests/greetings_test.rs
@@ -9,3 +9,14 @@ fn test_hello() {
     // THEN
     assert_eq!(got, expected_greeting);
 }
+
+#[test]
+fn test_welcome() {
+    // GIVEN
+    let person_name = "Lee";
+    let expected_greeting = String::from("Welcome Lee");
+    // WHEN
+    let got: String = hi::welcome(person_name);
+    // THEN
+    assert_eq!(got, expected_greeting);
+}
